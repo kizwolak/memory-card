bbe2fae4b402285c37e031f8cfe61037294b9ca8
kizwolak <ki.zwolak@gmail.com>
2023-02-24 12:25:38 -0300

    Generate cards as array and pass key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card';
+import Board from './components/Board';
 
 export default function App() {
   return (
-    <Card img={'./images/lampard.webp'} name={'Lampard'}/>
+    <Board />
   )
 }
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 export default function Board(props) {
-    
+    const [cardArray, setCardArray] = useState([
+        <Card img={'./images/lampard.webp'} name={'Lampard'} key={'Lampard'}/>,
+        <Card img={'./images/hazard.webp'} name={'Hazard'} key={'Hazard'}/>,
+        <Card img={'./images/zola.webp'} name={'Zola'} key={'Zola'}/>,
+        <Card img={'./images/carvalho.webp'} name={'Carvalho'} key={'Carvalho'}/>,
+        <Card img={'./images/cole.webp'} name={'Cole'} key={'Cole'}/>,
+        <Card img={'./images/drogba.webp'} name={'Drogba'} key={'Drogba'}/>,
+        <Card img={'./images/ballack.webp'} name={'Ballack'} key={'Ballack'}/>,
+        <Card img={'./images/osgood.webp'} name={'Osgood'} key={'Osgood'}/>,
+        <Card img={'./images/terry.webp'} name={'Terry'} key={'Terry'}/>,
+        <Card img={'./images/ivanovic.webp'} name={'Ivanovic'} key={'Ivanovic'}/>,
+        <Card img={'./images/mikel.webp'} name={'Mikel'} key={'Mikel'}/>,
+        <Card img={'./images/anelka.webp'} name={'Anelka'} key={'Anelka'}/>,
+    ])
+    const generateArray = cardArray.map(card => {return card})
+    return (
+        <div className='board'>
+            {generateArray}
+        </div>
+    )
 }
